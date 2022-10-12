87d535f16f2ddab27de9156884ebdc1d93ddcc22
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2022-10-13 03:29:38 +0700

    main

--- Exercise-5/Program.cs
+++ Exercise-5/Program.cs
@@ -10,6 +10,7 @@ namespace Exercise_5
     {
         static void Main(string[] args)
         {
+            // ข้อ 4.2
             int mark = 0, i = 1;
             Dice dic1 = new Dice();
 
@@ -20,9 +21,27 @@ namespace Exercise_5
                 System.Threading.Thread.Sleep(10);
                 i += 1;
             }
+
             Console.WriteLine("Throw of dice = 1 is: " + (i - 1) + " time");
             Console.ReadLine();
 
+            // ข้อ 4.3
+            /*int mark1 = 0, mark2 = 0, i = 1;
+            Dice dic1 = new Dice();
+            Dice dic2 = new Dice();
+
+            while (mark1 != 1 && mark2 != 1)
+            {
+                mark1 = dic1.Throw();
+                mark2 = dic2.Throw();
+
+                Console.WriteLine("{0} Mark of Dice: {1} and {2}", i, mark1, mark2);
+                System.Threading.Thread.Sleep(10);
+                i += 1;
+            }
+
+            Console.WriteLine("Throw of dice = 1 is: " + (i - 1) + " time");
+            Console.ReadLine();*/
         }
     }
 }
